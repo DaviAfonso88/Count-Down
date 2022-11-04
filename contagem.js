abfd96f969f24b66fb169d9e9120ed1aea202de5
DaviAfonso88 <daviafonso5894@gmail.com>
2022-11-04 13:58:35 -0300

    contagem regressiva

---
+++ contagem.js
@@ -0,0 +1,51 @@
+
+
+function start(){
+    intervalo = setInterval(contagemRegressiva(tempoRestante()))
+ }
+
+
+
+var formatarDigito = (digito) => `0${digito}`.slice(-2);
+var atualizar = (tempo) =>{
+    const segundos = document.getElementById('segundos');
+    const minutos = document.getElementById('minutos');
+    const horas = document.getElementById('horas');
+    const dias = document.getElementById('dias');
+
+
+    const qtdSegundos = tempo % 60;
+    const qtdMinutos = Math.floor(tempo % (60 * 60) / 60);
+    const qtdHoras = Math.floor(tempo % (60 * 60 * 24) / (60 * 60));
+    const qtdDias = Math.floor(tempo / (60 * 60 * 24));
+
+    segundos.textContent = formatarDigito(qtdSegundos);
+    minutos.textContent = formatarDigito(qtdMinutos);
+    horas.textContent = formatarDigito(qtdHoras);
+    dias.textContent = formatarDigito(qtdDias);
+
+}
+
+var contagemRegressiva = (tempo) =>{
+    var pararContagem = () => clearInterval(id);
+
+    var contar = () => {
+        if (tempo == 0){
+            pararContagem();
+        }
+        atualizar (tempo);
+        tempo--;
+    }
+    var id = setInterval(contar, 1000);
+    
+    
+}
+var tempoRestante = (function(hoje, dataEvento){
+    var dataEvento = new Date ('2022-11-05 20:00:00');
+    var hoje = Date.now();
+    return Math.floor((dataEvento - hoje) / 1000);
+
+})
+
+// contagemRegressiva(tempoRestante());
+
