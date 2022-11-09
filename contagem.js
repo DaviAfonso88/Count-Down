613115ba3952f6efe933927c8c791782277c324a
DaviAfonso88 <daviafonso5894@gmail.com>
2022-11-09 13:53:38 -0300

    stop end alert

--- contagem.js
+++ contagem.js
@@ -1,19 +1,22 @@
+const segundos = document.getElementById('segundos');
+const minutos = document.getElementById('minutos');
+const horas = document.getElementById('horas');
+const dias = document.getElementById('dias');
+
+const timeRemainingInSeconds = 10;
+let timerIsRunning = false;
 
 
-function start(){
-    intervalo = setInterval(contagemRegressiva(tempoRestante()))
- }
+function start() {
+    if (!timerIsRunning)
+        contagemRegressiva(tempoRestante());
 
+    timerIsRunning = true;
+}
 
+let formatarDigito = (digito) => `0${digito}`.slice(-2);
 
-var formatarDigito = (digito) => `0${digito}`.slice(-2);
-var atualizar = (tempo) =>{
-    const segundos = document.getElementById('segundos');
-    const minutos = document.getElementById('minutos');
-    const horas = document.getElementById('horas');
-    const dias = document.getElementById('dias');
-
-
+let atualizar = (tempo) => {
     const qtdSegundos = tempo % 60;
     const qtdMinutos = Math.floor(tempo % (60 * 60) / 60);
     const qtdHoras = Math.floor(tempo % (60 * 60 * 24) / (60 * 60));
@@ -23,31 +26,34 @@ var atualizar = (tempo) =>{
     minutos.textContent = formatarDigito(qtdMinutos);
     horas.textContent = formatarDigito(qtdHoras);
     dias.textContent = formatarDigito(qtdDias);
-
 }
 
-var contagemRegressiva = (tempo) =>{
-    var pararContagem = () => clearInterval(id);
-
-    var contar = () => {
-        if (tempo == 0){
-            pararContagem();
+let contagemRegressiva = (tempo) => {
+    let interval = setInterval(() => {
+        if (tempo === 0 || Date.now() <= tempo) {
+            clearInterval(interval);
+            alert("O tempo acabou!");
+            timerIsRunning = false;
         }
-        atualizar (tempo);
+
+        atualizar(tempo);
         tempo--;
-    }
-    var id = setInterval(contar, 1000);
-    
-    
+    }, 1000);
 }
-var tempoRestante = () => {
-    var dataEvento = new Date ('2022-11-05 20:00:00');
-    var hoje = Date.now();
+
+let tempoRestante = () => {
+    let dataEvento =  new Date ('2022-11-09 20:00:00');
+    let hoje = Date.now();
     return Math.floor((dataEvento - hoje) / 1000);
 }
- 
 
+function stop() {
+    var r = confirm("--CONFIRMAR ENCERRAMENTO--");
+    if (r == true) {
+        clearInterval(timerIsRunning);
+        clearInterval(a);
 
-
-// contagemRegressiva(tempoRestante());
-
+    } else if (r == false) {
+        return interval;
+    } 
+}
